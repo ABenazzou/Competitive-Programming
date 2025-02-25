790d36883a2e0cb45609cecfc3ac89f547ff98b3
ABenazzou <adnanbenzo194@gmail.com>
2025-02-25 17:52:29 +0100

    Contest

---
+++ Codeforces.2072/B.cpp
@@ -0,0 +1,114 @@
+
+/*
+ * Author: @adnanebenazzou
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+//#define LOCAL
+#define all(a) (a).begin(), (a).end()
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef long double ld;
+
+const int INF = 1e9;
+const ll LLINF = 4e18;
+const double EPS = 1e-9;
+const ll MOD = 1e9 + 7;
+
+ll power(ll x, int exp, ll m)
+{
+    x %= m;
+    ll res = 1;
+    while (exp)
+    {
+	// if odd x ^ exp = x * x ^ exp - 1
+        if (exp & 1) res = res * x % m;
+        x = x * x % m;
+        exp >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+	int n;
+	string s;
+	cin >> n >> s;
+	int n1 = count(s.begin(), s.end(), '-'), n2 = count(s.begin(), s.end(), '_');
+	if (n1 < 2 || n2 < 1) {
+		cout << 0 << '\n';
+		return;
+	}
+	ll res = (n1 + 1) / 2;
+	res *= n2 * floor(n1 / 2);
+	cout << res << '\n';
+
+	/*
+	string best = "";
+	for (int i = 0; i < (n1 + 1) / 2; i ++) {
+		best += "-";
+	}
+	n1 -= (n1 + 1)/2;
+	for (int i = 0; i < n2; i ++) {
+		best += "_";
+	}
+	for (int i = 0; i < n1 ; i ++) {
+		best += "-";
+	}
+	dbg(best);
+	vi freq(n, 0);
+	for ( int i = n - 2; i >= 0; i --){
+		freq[i] = freq[i + 1];
+		if (best[i + 1] == '-') freq[i] = freq[i] + 1;
+	}
+
+	for (int i = 0; i < n; i ++) {
+		if (best[i] != '-') break; // finished
+		bool in = false;
+		for (int j = i + 1; j < n; j ++) {
+			if (best[j] == '_') {
+				res += freq[j]; // how many - possible  after it
+				in = true;
+			}
+			else if (in) break;
+
+		}
+	}
+	cout << res << '\n';
+	*/
+
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int tc = 1;
+    cin >> tc;
+    //scanf("%d", &tc);
+    
+    for (int i = 1; i <= tc; i ++ ) {
+		dbg("Test Case: ", i);
+		solve();
+	}
+
+	return 0;
+}
+
