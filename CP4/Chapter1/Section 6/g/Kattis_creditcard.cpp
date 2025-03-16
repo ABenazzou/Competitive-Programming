be6d745eb68113cf72d0bf6afafa844f570ee7b2
ABenazzou <adnanbenzo194@gmail.com>
2025-03-16 21:40:40 +0100

    CP4 - Ch1 - S6 - g

---
+++ CP4/Chapter1/Section 6/g/Kattis_creditcard.cpp
@@ -0,0 +1,93 @@
+
+/*
+ * Author: @adnanebenazzou
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+//#define LOCAL
+#define all(a) (a).begin(), (a).end()
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef long double ld;
+
+const int INF = 1e9;
+const ll LLINF = 4e18;
+const double EPS = 1e-9;
+const ll MOD = 1e9 + 7;
+
+ll power(ll x, int exp, ll m)
+{
+    x %= m;
+    ll res = 1;
+    while (exp)
+    {
+	// if odd x ^ exp = x * x ^ exp - 1
+        if (exp & 1) res = res * x % m;
+        x = x * x % m;
+        exp >>= 1;
+    }
+    return res;
+}
+
+void solve() {
+	double r, b, m;
+	cin >> r >> b >> m;
+	r /= 100.0;
+
+	bool ok = false;
+	int i;
+	for (i = 0; i < 1200; i ++) {
+		double interest = b * r;
+		interest = round(interest * 100) / 100; // round to nearest cent and / 100 back
+		b += interest;		
+		b = round(b * 100) / 100; 
+		b -= m;
+		if (b <= 0) {
+			ok = true;
+			break;
+		}
+	}
+	if (ok) {
+		cout << i + 1 << '\n';
+	} else {
+		cout << "impossible\n";
+	}
+
+	
+	
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    int tc = 1;
+    cin >> tc;
+    //scanf("%d", &tc);
+    
+    for (int i = 1; i <= tc; i ++ ) {
+		dbg("Test Case: ", i);
+		solve();
+	}
+
+	return 0;
+}
+
